b199e2c92450afbf2c5b17f6532f98092143f21b
Muhammad Saad Zubair <muhammadsaadzubair186@gmail.com>
2025-08-16 16:16:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,22 @@ def load_model():
 model = load_model()
 
 # Streamlit UI
-st.title("Cat/Dog Classifier")
-st.write("Upload an image and let the model classify it as a cat or dog.")
+st.title("Cat vs Dog Classifier")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Open and preprocess image
+    # Preprocess image
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
-    img_resized = image.resize((128, 128))  # resize same as training size
+    img_resized = image.resize((128, 128))  # same as training size
     img_array = np.array(img_resized) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     # Prediction
     prediction = model.predict(img_array)[0][0]
 
+    # Output
     if prediction > 0.5:
-        st.success(f"Prediction: Dog ")
+        st.write("### Prediction: Dog")
     else:
-        st.error(f"Prediction: Cat ")
+        st.write("### Prediction: Cat")
